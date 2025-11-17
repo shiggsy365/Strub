e828afc8924443888cbacab652a88af0fd4fc49f
shiggsy365-maker <shiggsy365@gmail.com>
2025-11-17 15:08:44 +0000

    error fixes

---
+++ app/src/main/java/com/example/stremiompvplayer/ui/MoviesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.stremiompvplayer.ui.movies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class MoviesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return TextView(context).apply {
+            text = "Movies - Coming Soon"
+            textSize = 24f
+            setTextColor(android.graphics.Color.WHITE)
+        }
+    }
+}
