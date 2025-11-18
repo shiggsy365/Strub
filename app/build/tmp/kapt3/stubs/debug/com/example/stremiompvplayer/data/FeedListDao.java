3966bc63090058092c45b8858ff0f5fb9fffb844
shiggsy365 <shiggsy365@gmail.com>
2025-11-18 09:45:30 +0000

    Delete app/build directory

--- app/build/tmp/kapt3/stubs/debug/com/example/stremiompvplayer/data/FeedListDao.java
+++
@@ -1,6 +0,0 @@
-package com.example.stremiompvplayer.data;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\bg\u0018\u00002\u00020\u0001\u00a8\u0006\u0002"}, d2 = {"Lcom/example/stremiompvplayer/data/FeedListDao;", "", "app_debug"})
-@androidx.room.Dao()
-public abstract interface FeedListDao {
-}
