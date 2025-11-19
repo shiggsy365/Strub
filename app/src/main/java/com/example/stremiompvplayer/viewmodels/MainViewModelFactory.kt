3dd37ca240d247d76003cfcb95b5fb7fa9d47aeb
shiggsy365 <shiggsy365@gmail.com>
2025-11-19 15:57:28 +0000

    Update MainViewModelFactory.kt

--- app/src/main/java/com/example/stremiompvplayer/viewmodels/MainViewModelFactory.kt
+++ app/src/main/java/com/example/stremiompvplayer/viewmodels/MainViewModelFactory.kt
@@ -3,12 +3,16 @@ package com.example.stremiompvplayer.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.stremiompvplayer.data.ServiceLocator
+import com.example.stremiompvplayer.utils.SharedPreferencesManager
 
-class MainViewModelFactory(private val serviceLocator: ServiceLocator) : ViewModelProvider.Factory {
+class MainViewModelFactory(
+    private val serviceLocator: ServiceLocator,
+    private val prefsManager: SharedPreferencesManager
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return MainViewModel(serviceLocator.catalogRepository) as T
+            return MainViewModel(serviceLocator.catalogRepository, prefsManager) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
